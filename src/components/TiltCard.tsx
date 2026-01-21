c7416989dfbf54aa8f74745cb865002f3fa2247f
egg <yhopl99008@gmail.com>
2026-01-21 11:56:49 +0400

    fix: position checkmark absolutely to prevent height increase on flip

--- src/components/TiltCard.tsx
+++ src/components/TiltCard.tsx
@@ -354,11 +354,13 @@ export const TiltCard = ({
 
                         <div className="relative z-10 w-full flex flex-col items-center gap-4">
                             {children}
-                            <div className="w-12 h-12 rounded-full bg-[#22c55e] flex items-center justify-center shadow-[0_0_30px_rgba(34,197,94,0.8)] border-2 border-white/20">
-                                <svg viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-white">
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                </svg>
-                            </div>
+                        </div>
+
+                        {/* Checkmark - absolutely positioned to not affect layout */}
+                        <div className="absolute bottom-6 w-12 h-12 rounded-full bg-[#22c55e] flex items-center justify-center shadow-[0_0_30px_rgba(34,197,94,0.8)] border-2 border-white/20">
+                            <svg viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-white">
+                                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                            </svg>
                         </div>
 
                         <motion.div
